af00043fe95e6829a5d8d80b9220a34a28ad1a94
param.unik <param.unik@gmail.com>
2022-06-19 10:16:16 +0530

    logger and exception

---
+++ housing/exception/__init__.py
@@ -0,0 +1,29 @@
+import os, sys 
+
+class HousingException(Exception):
+
+    def __init__(self, error_message: Exception, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = HousingException.get_detailed_error_message(error_message=error_message,
+                                                        error_details=error_details)
+    
+    @staticmethod
+    def get_detailed_error_message(error_message: Exception, error_details:sys) -> str :
+
+        '''
+        error_message:  Exception object 
+        error-details:  object of sys module
+
+        '''
+        _, _ , exec_trace = error_details.exc_info() 
+        line_number = exec_trace.tb_frame.f_lineno
+        file_name = exec_trace.tb_frame.f_code.co_filename
+        error_message = f"Error occured in script :[{file_name}] at line number : [{line_number}]. Error message : [{error_message}]"
+
+        return error_message
+
+    def __str__(self):
+        return self.error_message
+    
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from tkinter.tix import DECREASING
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
@@ -8,7 +7,6 @@ PROJECT_NAME = 'housing predictor'
 VERSION = '0.0.1'
 AUTHOR = 'Param'
 DESCRIPTION = 'This is a first FSDS batch NOV batch'
-PACKAGES = ['housing']
 REQUIREMENTS_FILE_NAME = 'requirements.txt'
 
 
@@ -20,16 +18,15 @@ def get_requirements_list() -> List[str]:
 
     '''
     with open(REQUIREMENTS_FILE_NAME) as req_file:
-        return req_file.readlines()
+        data =  req_file.readlines()
+
+    return data.remove('-e .')
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
-
-# if __name__ == '__main__':
-#     print(get_requirements_list())
